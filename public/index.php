e62bf77f11fe24e100f948166ebc17a254c95c1a
ramones156 <ramonvansprundel@gmail.com>
2022-07-03 01:51:44 +0200

    .gitignore

--- public/index.php
+++ public/index.php
@@ -4,9 +4,6 @@ use App\Kernel;
 
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 
-header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Methods: GET, PUT, POST, DELETE, OPTIONS');
-
 return function (array $context) {
     return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
 };
